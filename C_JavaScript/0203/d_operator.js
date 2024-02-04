8db1d6a6b005945737fd29cfb60ffd0829c71db7
devgiants75 <devgiants75@naver.com>
2024-02-04 14:37:08 +0900

    20240204

--- C_JavaScript/0203/d_operator.js
+++ C_JavaScript/0203/d_operator.js
@@ -102,4 +102,22 @@ console.log(num1 < num2); // 미만 - false
 console.log(num1 >= num2); // 이상 - true
 console.log(num1 <= num2); // 이하 - true
 
-//!
+//! 논리 연산자
+// : boolean 값을 연산하는데 사용
+
+// and(논리곱): &&
+// : false값이 하나라도 있으면 무조건 false
+
+// or(논리합): ||
+// : true값이 하나라도 있으면 무조건 true
+
+// not(부정논리): !
+
+let bool1 = true;
+let bool2 = false;
+
+console.log(bool1 && bool2); // false
+console.log(bool1 || bool2); // true
+
+console.log(!bool1); // false
+console.log(!bool2); // true
