8d83c8d8e50f8b50222f9acd700613a1d306df1f
devgiants75 <devgiants75@naver.com>
2024-02-25 17:49:02 +0900

    20240225

--- C_JavaScript/0225/practice/main.js
+++ C_JavaScript/0225/practice/main.js
@@ -4,7 +4,8 @@ let items = []; // 추가되는 li를 저장
 //! 새 아이템을 생성하는 함수(createItem)
 function createItem() {
   // 입력 필드의 DOM 요소를 읽어오기
-  const inputField = document.getElementById('input-field');
+  const inputField = document.getElementById("input-field");
+
   // 입력 필드의 값을 가져오기
   // : 공백 제거 후 가져오기
   const newItem = inputField.value.trim();
@@ -15,7 +16,7 @@ function createItem() {
     items = [...items, newItem];
 
     // 입력 필드 초기화
-    inputField.value = '';
+    inputField.value = "";
 
     // 아이템 목록을 다시 렌더링(renderItems)
     renderItems();
@@ -33,13 +34,14 @@ function deleteItem(index) {
   // 3. 배열 그 자체
 
   // 콜백함수의 인자로 사용하지 않는 우선순위 매개변수는 _언더바로 표시
-  items = items.filter((_, i) =>
-    // 현재 처리 중인 요소의 인덱스가(i)
-    // 매개변수(index)와 다른 경우에만 true를 반환
-    // >> 새로운 배열의 요소로 저장
-    // 인덱스 번호가 3번인 요소를 삭제하는 경우
-    // : 배열을 순회하면서 3번인 요소만 제외하고 새로운 배열로 반환 (삭제된 효과)
-    i !== index
+  items = items.filter(
+    (_, i) =>
+      // 현재 처리 중인 요소의 인덱스가(i)
+      // 매개변수(index)와 다른 경우에만 true를 반환
+      // >> 새로운 배열의 요소로 저장
+      // 인덱스 번호가 3번인 요소를 삭제하는 경우
+      // : 배열을 순회하면서 3번인 요소만 제외하고 새로운 배열로 반환 (삭제된 효과)
+      i !== index
   );
 
   // 아이템 목록을 다시 렌더링
@@ -49,25 +51,26 @@ function deleteItem(index) {
 //! 아이템 목록을 렌더링하는 함수(renderItems)
 function renderItems() {
   // 아이템 목록을 담을 DOM 요소를 읽어오기
-  const itemList = document.getElementById('item-list');
+  const itemList = document.getElementById("item-list");
   // 목록을 초기화 (innerHTML: 내부 HTML코드를 불러오기)
-  itemList.innerHTML = '';
+  itemList.innerHTML = "";
 
   // 아이템 배열을 순회하여 목록을 생성(map)
-  items.map((item, index) => {
+  items.forEach((item, index) => {
     // 각각의 아이템에 대한 li요소를 생성
-    const li = document.createElement('li');
+    const li = document.createElement("li");
 
     // li 요소에 아이템 텍스트 추가
     li.textContent = item;
+    console.log("텍스트 추가 성공");
 
     // li 요소 클릭 시 해당 아이템 삭제 함수를 호출
     li.onclick = () => deleteItem(index);
 
     // 생성한 li요소를 목록에 추가
     itemList.appendChild(li);
-  })
+  });
 }
 
 // 문서가 로드되었을 때 아이템 목록을 처음으로 렌더링
-document.addEventListener('DOMContentLoaded', renderItems);
+document.addEventListener("DOMContentLoaded", renderItems());
