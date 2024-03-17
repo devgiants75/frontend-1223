d3b6724c9ee975374a7f334589e099beb071b598
devgiants75 <devgiants75@naver.com>
2024-03-17 17:40:52 +0900

    20240317

---
+++ D_TypeScript/src/0317/z_answer.ts
@@ -0,0 +1,1 @@
+// z_answer.ts
---
+++ D_TypeScript/src/0317/z_practice.ts
@@ -0,0 +1,13 @@
+// z_practice.ts
+
+// 1. Union 타입을 사용한 변수 선언
+// string, number, boolean 중 하나의 타입을 가질 수 있는 MixedType 변수를 선언하고, 각 타입에 해당하는 값을 할당하는 예시 코드를 작성
+
+// 2. 함수 매개변수에 Union 타입 적용
+// number와 string 타입 중 하나를 매개변수로 받아, 해당 값이 number일 경우 숫자를 2배로 증가시키고, string일 경우 그대로 반환하는 함수 doubleOrNothing을 작성
+
+// 3. Union 타입과 타입 가드를 활용한 고급 예제
+// Admin과 User 타입 명시
+// - Admin은 id (number 타입)와 isAdmin (boolean 타입) 속성을, User는 id (number 타입)와 username (string 타입) 속성 포함
+// - 두 타입의 유니온 타입을 사용하여 Person 타입을 선언하고, id, isAdmin, username 중 적절한 속성을 가진 객체를 생성
+// - Person 타입의 객체를 매개변수로 받아 Admin인지 User인지를 구분해 출력하는 함수 identifyPerson을 작성
