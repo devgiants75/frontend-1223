9644426ae2661c3dce189b69d676cbba5e5d72a0
devgiants75 <devgiants75@naver.com>
2024-03-23 16:43:34 +0900

    20240323

--- D_TypeScript/src/0323/d_function.ts
+++ D_TypeScript/src/0323/d_function.ts
@@ -56,4 +56,79 @@ console.log(select('lsa', 'hello')); // hello lsa
 // 3. 기본 매개변수가 필수 매개변수 앞에 작성된 경우
 // : 기본 매개변수를 생략하려면 반드시 undefined를 전달
 console.log(select(undefined, 'hello')); // hello
+
+//! Rest 매개변수
+// : 함수에 여러 개의 매개변수를 그룹화하여 배열로 전달하는 기능
+// : 함수에 가변적인 수의 인자를 안전하고 효율적으로 처리하도록 하는 기능
+
+// ...연산자를 매개변수 앞에 사용하여 정의
+function sum(...numbers: number[]): number {
+  // reduce 메서드
+  // : 네 개의 매개변수를 가질 수 있는 콜백함수
+  // - 1. accumulator: 누적 계산의 결과값
+  // - 2. currentValue: 처리할 현재 요소
+  // - 3. (선택) 처리할 현재 요소의 인덱스
+  // - 4. (선택) reduce를 호출한 배열
+  return numbers.reduce((a, b) => a + b, 0);
+  // a += b
+}
+
+const result1 = sum(1, 2, 3, 4, 5);
+console.log(result1); // 15
+const result2 = sum(10, 20, 30);
+console.log(result2); // 60
+const result3 = sum(129, 3242, 234);
+console.log(result3); // 3605
+
+// 첫 번째 인자는 name의 변수에
+// 두 번째 인자부터는 names 배열에 할당
+function greetEveryone(name: string, age: number, ...names: string[]): string {
+  // join 메서드
+  // : 배열의 모든 요소를 연결하여 하나의 문자열로 생성
+  // : 배열 내의 각 요소를 문자열로 변환하고 지정된 구분자(선택사항)로 해당 요소들을 구분하여 결합
+  return names
+    .map(name => `Hello ${name}`)
+    .join(', ');
+  // : 메서드 체이닝
+}
+
+console.log(greetEveryone('이승아', 30, '이도경', '진현지', '심은혜')); // Hello 이도경, Hello 진현지, Hello 심은혜
+
+// Rest 매개변수 주의사항
+// : 항상 마지막에 위치
+// : 타입 명시를 주의
+
+//! 함수의 '오버로딩'
+// : 같은 이름의 함수에 대해 
+//   여러 매개변수의 타입과 반환 타입을 정의하는 기능
+
+//? 특징
+// : 함수는 '하나 이상의 타입 시그니처'를 가질 수 있다.
+// : 함수는 '단 하나의 구현'을 가질 수 있다.
+
+// 기본 문법
+// : 함수 선언부는 여러 개가 될 수 있지만, 구현은 하나만 있어야 한다.
+// - 함수의 선언부: 중괄호 이전의 함수명, 매개변수 지정
+// - 함수의 구현부: 중괄호와 중괄호 내의 코드
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
+
+function add(a: any, b: any): any {
+  // 함수 구현부: 단일 함수명에 대한 중괄호는 반드시 하나!
+  if (typeof a === 'number' && typeof b === 'number') {
+    return a + b;
+  } else if (typeof a === 'string' && typeof b === 'string') {
+    return a.concat(b);
+    // concat() 메서드
+    // : 매개변수로 전달된 모든 문자열을 호출 문자열에 붙인 새로운 문자열을 반환
+  }
+  throw new Error('Invalid arguments');
+}
+
+console.log(add(1, 2)); // 3
+console.log(add('이', '승아')); // 이승아
+
+// 함수의 오버로딩의 사용
+// : 함수에 대한 다양한 타입의 매개변수를 제공할 수 있도록 지정
+// : + 타입 안정성 유지
 }
---
+++ D_TypeScript/src/0323/f_practice.ts
@@ -0,0 +1,15 @@
+// f_practice.ts
+
+// 1. 간단한 사용자 관리 시스템
+
+// 요구 사항
+//? User 인터페이스 생성
+// id: 숫자 타입
+// username: 문자열 타입
+// role: "admin" 또는 "user" 리터럴 타입
+
+//? createUser 함수 타입을 정의
+// username (문자열), role ("admin" 또는 "user" 리터럴)을 매개변수로 받고, User 객체를 반환
+
+//? updateUserRole 함수 타입을 정의
+// user (User 객체), newRole ("admin" 또는 "user" 리터럴)을 매개변수로 받고, 업데이트된 User 객체를 반환
