a3de908794cd0784bbde2e5280cef4e86c5de72d
devgiants75 <devgiants75@naver.com>
2024-04-13 17:50:39 +0900

    20240413

--- e-react/src/views/0413/c_component/Component03.tsx
+++ e-react/src/views/0413/c_component/Component03.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import puppy02 from '../../../assets/images/puppy02.jpg'
 
 // puppy02 이미지를 생성
 // : 픽사베이에서 이미지 다운로드
@@ -10,6 +11,8 @@ import React from 'react'
 // Component03 컴포넌트를 index.tsx 파일에서 호출(사용)
 export default function Component03() {
   return (
-    <div>Component03</div>
+    <div>
+      <img src={puppy02} alt="강아지02" width={200} />
+    </div>
   )
 }
--- e-react/src/views/0413/index.tsx
+++ e-react/src/views/0413/index.tsx
@@ -4,6 +4,7 @@ import React02 from './a_React/React02'
 import Route from './b_Route/Route'
 import Component01 from './c_component/Component01'
 import Component02 from './c_component/Component02'
+import Component03 from './c_component/Component03'
 
 // 0413 폴더 내의 전체 컴포넌트를 담는 폴더
 export default function Index() {
@@ -19,6 +20,7 @@ export default function Index() {
       <h2>c_Component</h2>
       <Component01 />
       <Component02 />
+      <Component03 />
     </>
   )
 }
