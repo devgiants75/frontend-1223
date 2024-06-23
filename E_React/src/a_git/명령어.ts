3cd8ba38c315f980e6c8ed3c32235f28d5ceea1d
devgiants75 <devgiants75@naver.com>
2024-06-23 16:20:00 +0900

    20240623

--- E_React/src/a_git/명령어.ts
+++ E_React/src/a_git/명령어.ts
@@ -11,7 +11,6 @@ $ cd repository-name
 : 원본 리포지토리의 변경 사항을 가져오기 위해 upstream 원격을 추가
 
 $ git remote add upstream https://github.com/original-owner/repository-name.git
------------------------------------------------------------------------------
 
 3. 브랜치 생성 및 작업
 : 각 팀원은 자신의 브랜치를 생성하고 그 브랜치에서 작업을 수행
@@ -19,12 +18,22 @@ $ git remote add upstream https://github.com/original-owner/repository-name.git
 브랜치 생성
 $ git checkout -b feature-branch-name
 
-작업 내용 커밋
+-----------------------------------------------------------------------------
+
+(
+작업 전
+$ git fetch upstream
+$ git checkout main
+$ git merge upstream/main
+$ git push origin main
+)
+
+=== 작업 내용 커밋 ===
 $ git add .
 $ git commit -m "작업 내용 설명"
 
-자신의 GitHub 리포지토리에 푸시
-$ git push origin feature-branch-name
+=== 자신의 GitHub 리포지토리에 푸시 ===
+$ git push origin feature-branch-name<개인브랜치명>
 
 4. 원본 리포지토리와 동기화
 
@@ -37,6 +46,8 @@ $ git checkout main
 원본 리포지토리의 변경 사항을 로컬 메일 브랜치에 병합
 $ git merge upstream/main
 
+$ git push origin main
+
 === 병합 후 충돌 해결 (필요한 경우) === 
 충돌이 발생하면 충돌된 파일을 수정한 후 스테이징하고 커밋
 
@@ -47,7 +58,7 @@ $ git commit -m "충돌 해결"
 5. 브랜치에 변경 사항 반영
 
 작업 브랜치로 돌아가기
-$ git checkout feature-branch-name
+$ git checkout feature-branch-name<개인브랜치명> 
 
 메인 브랜치의 변경 사항을 작업 브랜치에 병합
 $ git merge main
